3775b906b2a2a942335ad76a66e0640d87033f9e
acse-ns1321 <88569855+acse-ns1321@users.noreply.github.com>
2022-01-14 10:27:17 +0000

    Worked on different containers in C++

---
+++ cpp/containers/1 - vectors.cpp
@@ -0,0 +1,38 @@
+#include <deque>
+
+std::vector<int> v;
+
+//---------------------------------
+// General Operations
+//---------------------------------
+
+// Size
+unsigned int size = v.size();
+
+// Insert head, index, tail
+v.insert(v.begin(), value);             // head
+v.insert(v.begin() + index, value);     // index
+v.push_back(value);                     // tail
+
+// Access head, index, tail
+int head = v.front();       // head
+head = v[0];                // or using array style indexing
+
+int value = v.at(index);    // index
+value = v[index];           // or using array style indexing
+
+int tail = v.back();        // tail
+tail = v[v.size() - 1];     // or using array style indexing
+    
+// Iterate
+for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+    std::cout << *it << std::endl;
+}
+
+// Remove head, index, tail
+v.erase(v.begin());             // head
+v.erase(v.begin() + index);     // index
+v.pop_back();                   // tail
+
+// Clear
+v.clear();
---
+++ cpp/containers/4. map.cpp
@@ -0,0 +1,35 @@
+std::map<std::string, std::string> m;
+
+//---------------------------------
+// General Operations
+//---------------------------------
+
+// Insert
+m.insert(std::pair<std::string, std::string>("key", "value"));
+
+// Access by key
+std::string value = m.at("key");
+
+// Size
+unsigned int size = m.size();
+
+// Iterate
+for(std::map<std::string, std::string>::iterator it = m.begin(); it != m.end(); it++) {
+    std::cout << *it << std::endl;
+}
+
+// Remove by key
+m.erase("key");
+
+// Clear
+m.clear();
+
+//---------------------------------
+// Container-Specific Operations
+//---------------------------------
+
+// Find if an element exists by key
+bool exists = (m.find("key") != m.end());
+
+// Count the number of elements with a certain key
+unsigned int count = m.count("key");
